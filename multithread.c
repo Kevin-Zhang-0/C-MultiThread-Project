f4004b052098802f39dacd0e8885a1e338c5b08b
Kevin Z <zhangk7532@gmail.com>
2021-10-12 18:32:45 -0400

    Structured Code

--- multithread.c
+++ multithread.c
@@ -10,15 +10,6 @@
 #include <dirent.h>
 #include <fcntl.h>
 #include <ctype.h>
-//#ifndef QSIZE
-//#define QSIZE 8
-//#endif
-
-
-
-
-
-
 
 int QSIZE = 20;
 
@@ -42,7 +33,6 @@ int init(queue_t *Q)
 	pthread_mutex_init(&Q->lock, NULL);
 	pthread_cond_init(&Q->read_ready, NULL);
 	pthread_cond_init(&Q->write_ready, NULL);
-	
 	return 0;
 }
 
@@ -51,7 +41,6 @@ int destroy(queue_t *Q)
 	pthread_mutex_destroy(&Q->lock);
 	pthread_cond_destroy(&Q->read_ready);
 	pthread_cond_destroy(&Q->write_ready);
-
 	return 0;
 }
 
@@ -61,37 +50,27 @@ int destroy(queue_t *Q)
 int enqueue(queue_t *Q, char* item)
 {
 	pthread_mutex_lock(&Q->lock);
-	
-	while (Q->count == QSIZE && Q->open) {
-        
+	while (Q->count == QSIZE && Q->open) {     
 		pthread_cond_wait(&Q->write_ready, &Q->lock);
 	}
 	if (!Q->open) {
 		pthread_mutex_unlock(&Q->lock);
 		return -1;
-	}
-	
+	}	
+
 	unsigned i = Q->head + Q->count;
    
 	if (i >= QSIZE-1){
-        
         QSIZE *= 2;
         char** temp = realloc(Q->data, sizeof(char*) * QSIZE);
-       
         Q->data = temp;
-        
     }
-	
-    //testing-kevin
+
     Q->data[i] = malloc(sizeof(char)*(strlen(item)+1));
     strcpy(Q->data[i],item);
-    //test
-
-	//Q->data[i] = item;
 	++Q->count;
 	
 	pthread_cond_signal(&Q->read_ready);
-	
 	pthread_mutex_unlock(&Q->lock);
 	
 	return 0;
@@ -110,11 +89,9 @@ int dequeue(queue_t *Q, char* item)
 		pthread_mutex_unlock(&Q->lock);
 		return -1;
 	}
-	
-    //item = Q->data[Q->head];
+
     strcpy(item, Q->data[Q->head]);
     free(Q->data[Q->head]);
-    //printf("i = %d queue data :%s",Q->head,item);
 	--Q->count;
 	++Q->head;
 	if (Q->head == QSIZE) Q->head = 0;
@@ -144,68 +121,6 @@ int length(queue_t *Q){
     pthread_mutex_unlock(&Q->lock);
     return p;
 }
-
-
-/* ******** 
- * Example client code for synchronized queue 
- */
-
-struct targs {
-	queue_t *Q;
-	int id;
-	int max;
-	int wait;
-};
-
-
-/**
-void *producer(void *A) 
-{
-	struct targs *args = A;
-	
-	int i, start, stop;
-	printf("[%d] begin producing %d\n", args->id, args->max);
-
-	start = args->id * 1000;
-	stop = start + args->max;
-	for (i = start; i < stop; ++i) {
-		printf("[%d] Sending %d\n", args->id, i);
-		if (enqueue(args->Q, i) != 0) {
-			printf("[%d] Enqueue failed\n", args->id);
-			return NULL;
-		};
-		sleep(args->wait);
-	}
-	
-	printf("[%d] finished\n", args->id);
-	
-	return NULL;
-}
-
-
-void *consumer(void *A)
-{
-	struct targs *args = A;
-	int i, j;
-	
-	printf("[%d] begin consuming %d\n", args->id, args->max);
-
-	for (i = 0; i < args->max; ++i) {
-		if (dequeue(args->Q, &j) != 0) {
-			printf("[%d] Dequeue failed\n", args->id);
-			return NULL;
-		}
-		printf("[%d] %d: Received %d\n", args->id, i, j);
-		sleep(args->wait);
-	}
-	
-	printf("[%d] finished\n", args->id);
-
-	return NULL;	
-}
-**/
-
-
 
 typedef struct{//arguments to pass into directory thread function
     queue_t *arg_d_q;
@@ -244,8 +159,6 @@ int increment(file__args *f){
     
 }
 
-
-
 void freeList(Node* head_ref){
  
     /* deref head_ref to get the real head */
@@ -276,11 +189,6 @@ void displayList(Node* h_ref){
     }
     
 }
-
-
-
-
-
 /*
 Inserts a word in alphabetical order into a linked list
 word2: is a string
@@ -303,7 +211,6 @@ Node * insertNode(char *word, Node *h,int size){
     newNode->freq = 0;
     newNode->word = NULL;
     newNode->next = NULL;
-
     if(h==NULL ){
         printf("head is nulasdasdasdadasdasl\n");
         h=malloc(sizeof(Node));
@@ -347,12 +254,8 @@ if no space on heap, malloc() will return NULL
             newNode->next = current;
         }
     }
-
-
     return h;
 }
-
-
 
 
 //dir checker
@@ -378,16 +281,11 @@ void *file_thread(void *temp){
     file_args *fstruct= (file_args *) temp;
     queue_t *f_q = (queue_t *)fstruct->file_arg_q;
     FileNode *fileArray = (FileNode *)fstruct->wsdFiles;
-
     Node *head = NULL;
-    //head->freq = 0;
-    //head->word = NULL;
-    //head->next = NULL;
-    
+ 
     int file_len = length(f_q);
     char*file_name = malloc(sizeof(char)* (file_len +10));
-    
-    //char* dir_name;
+
         
     dequeue(f_q,file_name);
 
@@ -413,21 +311,16 @@ void *file_thread(void *temp){
             
         }
         //space
-        else if(isspace(c)){
-            //printf("GOT HERE");
+        else if(isspace(c)){          
             //put into node
             buffer[buff_curr_size] = '\0';
             //first check if word exists in linked list
             //if yes increment count by 1
             //if not make new node and insert it in alphabetical order
-            //
-            //int inList = 0;
 
-            
             Node *ptr = head;
             int orig = 1;
-            //printf("word found");
-            
+
             while(ptr != NULL){
                 if (ptr->word != NULL){
                     if(strcmp(buffer, ptr->word) == 0){
@@ -452,23 +345,11 @@ void *file_thread(void *temp){
             for(int i=0; i<buff_curr_size; i++){//resets word buffer to be empty
                 buffer[i] = '\0';
             }
-            
-
-
             buff_curr_size=0;
-
-
-
         }
-        //punc
         else{
             continue;
         }
-
-
-
-
-        //printf("%c",c);
     }
     displayList(head);
 
@@ -494,50 +375,25 @@ void *dir_thread(void *q){
         if(d_q->count ==0){
             pthread_exit(NULL);
         }
-        
-        
-        
-        //int dir_len = strlen(d_q->data[d_q->head]);
         int dir_len = length(d_q);
-
-
         char*dir_name = malloc(sizeof(char)* (dir_len +10));
         char*original_dir_name = malloc(sizeof(char)* (dir_len +10));
-        //char* dir_name;
-        
         dequeue(d_q,dir_name);
-
-
-        
         strcpy(original_dir_name,dir_name);
-        //printf("o_dir_name:%s \n",dir_name);
-        
-        
-        
-        
         struct dirent *dp;
         DIR *dfd;
-        //error if cannot open directory?
-        
         dfd = opendir(dir_name);
-        
-        
-        
         while((dp= readdir(dfd))!=NULL){
-            
             strcpy(dir_name,original_dir_name);
             if(strcmp(dp->d_name,".")==0||strcmp(dp->d_name,"..")==0){
-                //printf("dont actually read \n");
                 continue;
             }
 
-            //printf("f in d : %s",dp->d_name);
             dir_name = realloc(dir_name,sizeof(char) * (dir_len + strlen(dp->d_name)+ 20));
             strcat(dir_name,"/");
             strcat(dir_name,dp->d_name);
             if(isdir(dir_name)){
-                
-                //printf("DIRECTORY FOUND, name created is: %s\n",dir_name);
+
                 enqueue(d_q,dir_name);
                 continue;
             }
@@ -555,9 +411,7 @@ void *dir_thread(void *q){
                     }
                 }
 
-                
                 if (sufSwitch == 1){
-                    printf("dir thread adds file to file q: %s\n",dir_name);
                     enqueue(f_q,dir_name);
                 }
                 
@@ -567,7 +421,6 @@ void *dir_thread(void *q){
         }
 
         closedir(dfd);
-
         free(original_dir_name);
         free(dir_name);
     }
@@ -577,8 +430,6 @@ void *dir_thread(void *q){
 
 
 int main(int argc, char **argv){
-
-    //printf("OQAWDJMIQOWD");
     queue_t dir_q;
     queue_t file_q;
     init(&dir_q);
@@ -607,24 +458,17 @@ int main(int argc, char **argv){
 
     //Checking parameters
     for(int i = 1;i<argc;i++){
-        
-        
+
         //if parameter
-        //printf("i is: %d\n", i);
         if(argv[i][0]== '-'){
             if (argv[i][1] == 'd' || argv[i][1] == 'f' || argv[i][1] == 'a'){
                 int total = 0;
-                
             
                 for (int j=2; j<strlen(argv[i]); j++){
                     int power = pow(10, strlen(argv[i])-j-1);
-                    //printf("power is: %d\n", power);
                     if (isdigit(argv[i][j]) == 0) return EXIT_FAILURE;
-                    //printf("digit: %c\n",argv[i][j]);
                     int tempNum = argv[i][j]- '0';
-                    //printf("tempNUm is: %d\n", tempNum);
                     total += (tempNum * power);
-                    //power = power / 10;
                 }
                 if (total <= 0) return EXIT_FAILURE;
                 else if (argv[i][1] == 'd') dN = total;
@@ -634,50 +478,30 @@ int main(int argc, char **argv){
             else if (argv[i][1] == 's'){
                 isSuffix = 1;
                 suffix = malloc(sizeof(char) * (strlen(argv[i])+1));
-                //memcpy(&suffix, &argv[i][2], strlen(argv[i]));
                 strcpy(suffix, argv[i]);
             }
             else{
                 printf("Error reading parameters");
                 return EXIT_FAILURE;
             }
-
             continue;
         }
         //queue
         //it is a directory
         if(isdir(argv[i])){
             enqueue(&dir_q,argv[i]);
-            
-            
         }
         //not a directory
         else{
             enqueue(&file_q,argv[i]);
             //add to file to file queue
         }
-        
     }
     if (isSuffix == 0){
         suffix = malloc(sizeof(char) * 5);
         strcpy(suffix, ".txt");
     }
     queues.suff = suffix;
-    
-    /**
-    for (int i=0; i<dir_q.count; i++){
-        printf("Directory is: %s\n", dir_q.data[i]);
-        
-        pthread_create(&thread_id,NULL,dir_thread,&dir_q);
-        pthread_join(thread_id, NULL);
-    }
-    
-    while(dir_q.count>0){
-        pthread_create(&thread_id,NULL,dir_thread,&queues);
-        pthread_join(thread_id, NULL);
-    }
-    **/
-    //unsigned int ids[dN];
     pthread_t tids[dN];
     for(int i =0;i<dN;i++){
         pthread_create(&tids[i],NULL,dir_thread,&queues);
@@ -687,34 +511,19 @@ int main(int argc, char **argv){
         
         pthread_join(tids[i], NULL);
     }
-
-    
-
     while(file_q.count>0){
         //printf("File is: %s\n", file_q.data[i]);
         
         pthread_create(&thread_id,NULL,file_thread,&file_thread_input);
         pthread_join(thread_id, NULL);
     }
-
-    
-    
     printf("dN is: %d\n", dN);
     printf("fN is: %d\n", fN);
     printf("aN is: %d\n", aN);
     printf("sN is: %s\n", suffix);
-    //temp
-    
-    //char* printme = malloc(sizeof(char) * 100);
-   
-
-   
-  
     free(WSD);
-
     free(suffix);
     qclose(&dir_q);
     qclose(&file_q);
-
     return EXIT_SUCCESS;
 }
